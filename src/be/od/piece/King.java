2857d971f00a7f0e80ab0baf0f3859af52836db9
Audric <onockx.a@gmail.com>
2020-12-17 21:01:09 +0100

    king and pawn (not diagonal)

--- src/be/od/piece/King.java
+++ src/be/od/piece/King.java
@@ -13,4 +13,31 @@ public class King extends Piece {
     public void tick() {
 
     }
+
+    @Override
+    public boolean canMove(int xDestination, int yDestination) {
+
+        int deltaX = xDestination - this.xSquare;
+        int deltaY = yDestination - this.ySquare;
+
+        if (Math.abs(deltaX) > 1 || Math.abs(deltaY) > 1) return false;
+        if(isCheck(xDestination, yDestination)) return false;
+
+        return true;
+    }
+
+    public boolean isCheck(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+                Piece tempPiece = board.getChessBoard()[i][j];
+                if(tempPiece!= null
+                && tempPiece.getPieceColor() != pieceColor
+                && tempPiece.canMove(x, y)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/be/od/piece/Pawn.java
+++ src/be/od/piece/Pawn.java
@@ -5,12 +5,34 @@ import be.od.board.Board;
 import java.awt.*;
 
 public class Pawn extends Piece{
+
+    boolean hasMoved;
     public Pawn(int x, int y, PieceColor pieceColor, Board board) {
         super(x, y, pieceColor, Type.PANW, board);
+        this.hasMoved = false;
     }
 
     @Override
     public void tick() {
 
+    }
+
+    @Override
+    public boolean canMove(int xDestination, int yDestination) {
+
+        int direction = pieceColor == PieceColor.WHITE ? 1 : -1;
+
+        int deltaX = (xDestination - this.xSquare) * direction;
+        int deltaY = (yDestination - this.ySquare) * direction;
+
+
+
+        if(deltaX != 0) return false;
+        if(deltaY < 0) return false;
+        if(hasMoved && deltaY > 1) return false;
+        if(!hasMoved && deltaY > 2) return false;
+
+        hasMoved = true;
+        return true;
     }
 }
